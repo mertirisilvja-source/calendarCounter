943b36e1a67ff8d7d04bb620b6e1b258a18be8e3
Silvja Fejzulla <mertirisilvja@gmail.com>
2026-01-15 11:33:33 +0100

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState, useMemo } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const newDate = useMemo(() => {
+    const currentDate = new Date();
+    return currentDate.setDate(currentDate.getDate() + count);
+  }, [count]);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          value={step}
+          min="0"
+          max="20"
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span> Step: {step} </span>
+      </div>
+      <br></br>
+      <div>
+        <button onClick={() => setCount((cnt) => cnt - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((cnt) => cnt + step)}>+</button>
+      </div>
+      <br></br>
+      <div>
+        {count === 0
+          ? "Today is "
+          : count > 0
+          ? `${count} days from today is `
+          : `${Math.abs(count)} days ago was}`}
+        <span>{new Date(newDate).toDateString()}</span>
+      </div>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
